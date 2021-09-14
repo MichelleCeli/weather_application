eb64f0ae8806072a7405e954efd878a587b123f5
Hallmann <michelle-celina.hallmann@stud.hshl.de>
2021-09-14 12:11:31 +0200

    homescreen ready, button needs function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { StartComponent } from './start/start.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    StartComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/start/start.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.scss']
+})
+export class StartComponent implements OnInit {
+
+  constructor() { }
+
+ /*  getSearchBar(): void {
+    console.log("searchbar opens");
+  } */
+
+  ngOnInit(): void {
+  }
+
+}
